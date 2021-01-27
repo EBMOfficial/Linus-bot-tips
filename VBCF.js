77d0a79995bc036adba0ed2825941238bd754218
EBMOfficial <63750451+EBMOfficial@users.noreply.github.com>
2021-01-27 13:27:40 -0500

    Update VBCF.js

--- VBCF.js
+++ VBCF.js
@@ -33,20 +33,45 @@ client.on('message', async koolMessage => {
   }
 });
 client.on('message', async message => {
-  if (message.content === `${prefix}vibecheck`) {
+  if(message.content.startsWith(prefix+'vibecheck')){
     var outcomes = ["0%", "1%", "2%", "3%", "4%", "5%", "6%", "7%", "8%", "9%", "10%", "11%", "12%", "13%", "14%", "15%", "16%", "17%", "18%", "19%", "20%", "21%", "22%", "23%", "24%", "25%", "26%", "27%", "28%", "29%", "30%", "31%", "32%", "33%", "34%", "35%", "36%", "37%", "38%", "39%", "40%", "41%", "42%", "43%", "44%", "45%", "46%", "47%", "48%", "49%", "50%", "51%", "52%", "53%", "54%", "55%", "56%", "57%", "58%", "59%", "60%", "61%", "62%", "63%", "64%", "65%", "66%", "67%", "68%", "69%", "70%", "71%", "72%", "73%", "74%", "75%", "76%", "77%", "78%", "79%", "80%", "81%", "82%", "83%", "84%", "85%", "86%", "87%", "88%", "89%", "90%", "91%", "92%", "93%", "94%", "95%", "96%", "97%", "98%", "99%", "100%",];
     let outcomesIndex = Math.round(Math.random() * outcomes.length);
-    const VibeEmbed = new Discord.MessageEmbed()
-    .setColor('RANDOM')
-  .setTitle(`${message.author.username} has a vibe percentage of:`)
-  .setDescription((outcomes[outcomesIndex]))
-  .setThumbnail('https://pbs.twimg.com/media/ECRsGT5U0AAmcVF.jpg')
-    .setTimestamp()
-    .setFooter('Very nice!');
-    message.channel.send(VibeEmbed)
-    console.log(`${message.author.username} got ${(outcomes[outcomesIndex])} on ${date}.`)
-  }
+    if(message.mentions.users.size){
+      let member=message.mentions.users.first()
+          if(member){
+            const VibeEmbed1 = new Discord.MessageEmbed()
+            .setColor('RANDOM')
+          .setTitle(`${member.username} has a vibe percentage of:`)
+          .setDescription((outcomes[outcomesIndex]))
+          .setThumbnail('https://pbs.twimg.com/media/ECRsGT5U0AAmcVF.jpg')
+            .setTimestamp()
+            .setFooter('Very nice!');
+              message.channel.send(VibeEmbed1)
+              console.log(`${member.username} got ${(outcomes[outcomesIndex])} on ${date}.`)
+              
+          }
+          else{
+              message.channel.send("Sorry, no one was found with that name.")
+  
+          }
+          }else{
+            const VibeEmbed = new Discord.MessageEmbed()
+            .setColor('RANDOM')
+          .setTitle(`${message.member.displayName} has a vibe percentage of:`)
+          .setDescription((outcomes[outcomesIndex]))
+          .setThumbnail('https://pbs.twimg.com/media/ECRsGT5U0AAmcVF.jpg')
+            .setTimestamp()
+            .setFooter('Very nice!');
+            message.channel.send(VibeEmbed)
+            console.log(`${message.author.username} got ${(outcomes[outcomesIndex])} on ${date}.`)
+          }
+  }  
 
 
-}); 
+
+
+
+
+
+});
 }
