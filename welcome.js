65086d55044af03bed5b49d66584e48ec33c3d9b
EBMOfficial <63750451+EBMOfficial@users.noreply.github.com>
2021-02-11 08:36:07 -0500

    Update welcome.js

--- welcome.js
+++ welcome.js
@@ -1,39 +1,35 @@
 const {
-prefix,
-} = require('./config.json')
-const Discord = require('discord.js')
-const { getChannelId } = require('./settingWelcome.js')
-module.exports = (client) => {
-    client.on("guildMemberAdd", (member) => {
-        // Destructure the guild property from the member object
-        const { guild } = member
-        // Access the channel ID for this guild from the cache
-        const channelId = getChannelId(guild.id)
-        console.log(member.id, member)
-        const AV = member.guild.roles.cache.find(role => role.name === "Awaiting Verification")
-        const BoomerRatInc = client.guilds.cache.get("597676585058828300")
-        if (member.guild === (BoomerRatInc)) {
-        member.roles.add(AV)
-        }
-        const G = member.guild.channels.cache.each(channel => channel.id === channelId);
-        if (G) {
-        const WelcomeChannel = guild.channels.cache.get(channelId)
-        WelcomeChannel.send(`Hey <@${member.id}>, Welcome to ${member.guild}! On behalf of the members of this server, I wish you a warm welcome!`)
-        } else return;
-     });
-     client.on('guildMemberRemove', (member) => {
-        // Destructure the guild property from the member object
-        const { guild } = member
-        // Access the channel ID for this guild from the cache
-        const channelId = getChannelId(guild.id)
-        console.log(member.id, member)
-        const G = member.guild.channels.cache.each(channel => channel.id === channelId)
-        if (G) {
-        const GoodbyeChannel = guild.channels.cache.get(channelId)
-        GoodbyeChannel.send(`Goodbye <@${member.id}>, we wish you have a great time away from us!`)
-        } else return;
-
-
-
-     });
-}
+   prefix,
+   } = require('./config.json')
+   const Discord = require('discord.js')
+   const { getChannelId } = require('./settingWelcome.js')
+   module.exports = (client) => {
+       client.on("guildMemberAdd", (member) => {
+           // Destructure the guild property from the member object
+           const { guild } = member
+           // Access the channel ID for this guild from the cache
+           const channelId = getChannelId(guild.id)
+           console.log(member.id, member)
+           const AV = member.guild.roles.cache.find(role => role.name === "Awaiting Verification")
+           const BoomerRatInc = client.guilds.cache.get("597676585058828300")
+           if (member.guild === (BoomerRatInc)) {
+           member.roles.add(AV)
+           }
+           const WelcomeChannel = member.guild.channels.cache.get(channelId)
+           if (WelcomeChannel) {
+           WelcomeChannel.send(`Hey <@${member.id}>, Welcome to ${member.guild}! On behalf of the members of this server, I wish you a warm welcome!`)
+           } 
+         
+        });
+        client.on('guildMemberRemove', (member) => {
+           // Destructure the guild property from the member object
+           const { guild } = member
+           // Access the channel ID for this guild from the cache
+           const channelId = getChannelId(guild.id)
+           console.log(member.id, member)
+           const GoodbyeChannel = member.guild.channels.cache.get(channelId)
+           if (GoodbyeChannel) {
+           GoodbyeChannel.send(`Goodbye <@${member.id}>, we wish you have a great time away from us!`)
+           } 
+        });
+   }
