16b88856d1a234a521672fe34682928930d6ff8f
EBMOfficial <63750451+EBMOfficial@users.noreply.github.com>
2021-02-27 12:56:00 -0500

    Update leaderboard.js

--- leaderboard.js
+++ leaderboard.js
@@ -3,9 +3,9 @@ const { Schema, model } = require('mongoose')
 const leaderboard = Schema({
   userid: String,
   guildID: String,
-  points: Number
+  points: Number,
+  awards: Number
 })
 
 module.exports = model('leaderboard', leaderboard)
  
-
